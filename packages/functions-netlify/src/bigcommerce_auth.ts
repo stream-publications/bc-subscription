d27005077b41ed5708744bccc275a52d9b6f770b
Jake He <zhex900@users.noreply.github.com>
2020-08-18 08:39:24 +1000

    add .env (#14)

--- packages/functions-netlify/src/bigcommerce_auth.ts
+++ packages/functions-netlify/src/bigcommerce_auth.ts
@@ -1,58 +1,63 @@
-require('dotenv').config()
+require("dotenv").config();
 
-import faunadb from 'faunadb'
+import * as faunadb from "faunadb";
 
-import * as BigCommerce from "node-bigcommerce"
+import * as BigCommerce from "node-bigcommerce";
 
-import { repository, models, interfaces } from "./bigcommerce/app"
-import { auth } from "./auth"
+import { repository, models, interfaces } from "./bigcommerce/app";
+import { auth } from "./auth";
 
 const faunadbClient = new faunadb.Client({
-    secret: process.env.FAUNADB_SERVER_SECRET
-})
+  secret: process.env.FAUNADB_SERVER_SECRET,
+});
 
 const bigCommerce = new BigCommerce({
-    logLevel: "debug",
-    clientId: process.env.BC_CLIENT_ID,
-    secret: process.env.BC_CLIENT_SECRET,
-    callback: process.env.BC_AUTH_CALLBACK,
-    responseType: "json",
-    apiVersion: "v3"
+  logLevel: "debug",
+  clientId: process.env.BC_CLIENT_ID,
+  secret: process.env.BC_CLIENT_SECRET,
+  callback: process.env.BC_AUTH_CALLBACK,
+  responseType: "json",
+  apiVersion: "v3",
 });
 
 exports.handler = async (event, context, callback) => {
-    console.info("Bang",event)
-    console.info(process.env.BC_CLIENT_ID)
-    try {
-        const authorizationData: interfaces.AuthResponse = await bigCommerce.authorize(event.queryStringParameters)
+  console.info("Bang", event);
+  console.info(process.env.BC_CLIENT_ID);
+  try {
+    const authorizationData: interfaces.AuthResponse = await bigCommerce.authorize(
+      event.queryStringParameters
+    );
 
-        const storeData: models.StoreData = {
-            access_token: authorizationData.access_token,
-            scope: authorizationData.scope,
-            context: authorizationData.context,
-            store_hash: authorizationData.context.split('/')[1],
-            owner_id: authorizationData.user.id,
-            active: true,
-            users: [authorizationData.user]
-        }
+    const storeData: models.StoreData = {
+      access_token: authorizationData.access_token,
+      scope: authorizationData.scope,
+      context: authorizationData.context,
+      store_hash: authorizationData.context.split("/")[1],
+      owner_id: authorizationData.user.id,
+      active: true,
+      users: [authorizationData.user],
+    };
 
-        await repository.createRecord(faunadbClient, storeData)
+    await repository.createRecord(faunadbClient, storeData);
 
-        return {
-            statusCode: 302,
-            headers: {
-                'Access-Control-Allow-Origin': '*',
-                'Cache-Control': 'no-cache',
-                'Set-Cookie': auth.generateCookie(storeData.store_hash, authorizationData.access_token),
-                'Location': process.env.APP_URL
-            },
-            body: ""
-        }
-    } catch (err) {
-        console.log(`Error: ${JSON.stringify(err)}`)
-        return {
-            statusCode: 500,
-            body: "Error Installing App"
-        }
-    }
-}
+    return {
+      statusCode: 302,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Cache-Control": "no-cache",
+        "Set-Cookie": auth.generateCookie(
+          storeData.store_hash,
+          authorizationData.access_token
+        ),
+        Location: process.env.APP_URL,
+      },
+      body: "",
+    };
+  } catch (err) {
+    console.log(`Error: ${JSON.stringify(err)}`);
+    return {
+      statusCode: 500,
+      body: "Error Installing App",
+    };
+  }
+};
